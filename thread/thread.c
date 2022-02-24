b1ad84996dac6f7357fb58e298ccb1ee01c4136d
L-Xiafeng <q1612188282@gmail.com>
2022-02-24 19:36:23 +0800

    multi thread and schule is OK

--- thread/thread.c
+++ thread/thread.c
@@ -2,11 +2,31 @@
 #include "stdint.h"
 #include "string.h"
 #include "global.h"
+#include "debug.h"
+#include "interrupt.h"
+#include "print.h"
 #include "memory.h"
 
 #define PG_SIZE 4096
 
+struct task_struct* main_thread;    // 主线程PCB
+struct list thread_ready_list;	    // 就绪队列
+struct list thread_all_list;	    // 所有任务队列
+static struct list_elem* thread_tag;// 用于保存队列中的线程结点
+
+extern void switch_to(struct task_struct* cur, struct task_struct* next);
+
+/* 获取当前线程pcb指针 */
+struct task_struct* running_thread(){
+    uint32_t esp;
+    asm ("mov %%esp, %0" : "=g" (esp));//esp指向PCB的页中
+    return (struct tast_struct*) (esp & 0xfffff000);
+}
+
+/* 由kernel_thread去执行function(func_arg) */
 static void kernel_thread(thread_func* function, void* func_arg ){
+    /* 执行function前要开中断,避免后面的时钟中断被屏蔽,而无法调度其它线程,导致线程独占CPU */
+    intr_enable();
     function(func_arg);
 }
 
@@ -14,16 +34,27 @@ static void kernel_thread(thread_func* function, void* func_arg ){
 void init_thread(struct task_struct* pthread, char* name, int prio) {
     memset(pthread,0,sizeof(*pthread));
     strcpy(pthread->name,name);
-    pthread->status = TASK_RUNNING;//FIXME此处仅为演示目的设置为RUNNGING
+
+    if (pthread == main_thread )
+    { 
+       pthread->status = TASK_RUNNING;
+    }else{
+        pthread->status = TASK_READY;
+    }
     pthread->priority = prio;
     //线程的内核栈初始化位PCB所在页的最高地址处
     pthread->self_kstack = (uint32_t*) ((uint32_t)pthread + PG_SIZE);
+    pthread->ticks = prio;
+    pthread->elapsed_ticks = 0;
+    pthread->pgdir = NULL;
     pthread->stack_magic = 0x20020905;//防止内核栈入栈过多破坏PCB低处的线程数据
 }
 
 /* 初始化线程栈thread_stack,将待执行的函数和参数放到thread_stack中相应的位置 */
 void thread_create(struct task_struct* pthread, thread_func function, void* func_arg) {
+    /* 先预留中断使用栈的空间,可见thread.h中定义的结构 */
     pthread->self_kstack -= (uint32_t)sizeof(struct intr_stack);
+    /* 再留出线程栈空间,可见thread.h中定义 */
     pthread->self_kstack -= (uint32_t)sizeof(struct thread_stack);
     struct thread_stack* kthread_stack = pthread->self_kstack;
     kthread_stack->eip = kernel_thread;
@@ -40,6 +71,60 @@ struct task_struct* thread_start(char* name, int prio, thread_func function, voi
     init_thread(thread,name,prio);
     thread_create(thread,function,func_arg);
 
-    asm volatile ("movl %0 , %%esp ; pop %%ebp;pop %%ebx; pop %%edi; pop %%esi; ret" : : "g"(thread->self_kstack) : "memory");
+    /* 确保之前不在队列中 */
+    ASSERT(!elem_find(&thread_ready_list,&thread->general_tag));
+    list_append(&thread_ready_list,&thread->general_tag);
+
+    /* 确保之前不在队列中 */
+    ASSERT(!elem_find(&thread_all_list,&thread->all_list_tag));
+    list_append(&thread_all_list,&thread->all_list_tag);
+
     return thread;
+}
+
+/* 将kernel中的main函数完善为主线程 */
+static void make_main_thread(void) {
+    /* 因为main线程早已运行,咱们在loader.S中进入内核时的mov esp,0xc009f000,
+就是为其预留了pcb,地址为0xc009e000,因此不需要通过get_kernel_page另分配一页*/
+    main_thread = running_thread();
+    init_thread(main_thread,"main",62);
+    
+    /* main函数是当前线程,当前线程正在运行，不在thread_ready_list中,
+ * 所以只将其加在thread_all_list中. */
+    ASSERT(!elem_find(&thread_all_list,&main_thread->all_list_tag));
+    list_append(&thread_all_list,&main_thread->all_list_tag);
+}
+
+/* 实现任务调度 */
+void schedule() {
+
+    //任务调度都一定在中断中（即关中断时进行）
+    ASSERT(intr_get_status() == INTR_OFF);
+
+    struct task_struct* cur = running_thread();
+    if (cur->status==TASK_RUNNING)
+    {
+        ASSERT(!elem_find(&thread_ready_list,&cur->general_tag));
+        list_append(&thread_ready_list,&cur->general_tag);
+        cur->ticks = cur->priority;
+        cur->status = TASK_READY;
+    }else {
+        /* 若此线程需要某事件发生后才能继续上cpu运行,
+      不需要将其加入队列,因为当前线程不在就绪队列中。*/
+    }
+
+    ASSERT(!list_empty(&thread_ready_list));
+    thread_tag = list_pop(&thread_ready_list);
+    struct task_struct* next = elem2entry(struct task_struct , general_tag , thread_tag);
+    next->status = TASK_RUNNING;
+    switch_to(cur,next);
+}
+
+/* 初始化线程环境 */
+void thread_init(void){
+    put_str("thread_init start\n");
+    list_init(&thread_all_list);
+    list_init(&thread_ready_list);
+    make_main_thread();
+    put_str("thread_init done\n");
 }
