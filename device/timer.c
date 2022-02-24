24b1f6221bc2e7b08d3893e126abad88aa043f8d
L-Xiafeng <q1612188282@gmail.com>
2022-02-24 19:28:04 +0800

    add sys_tick record,intr_timer_handler for thread switching and reistered it

--- device/timer.c
+++ device/timer.c
@@ -1,6 +1,9 @@
 #include "timer.h"
 #include "io.h"
 #include "print.h"
+#include "interrupt.h"
+#include "thread.h"
+#include "debug.h"
 
 #define IRQ0_FREQUENCY	    100                                 //我们要得到的输出频率
 #define INPUT_FREQUENCY	    1193180                             //8253的输入频率
@@ -11,6 +14,7 @@
 #define READ_WRITE_LATCH    3                                   //读写方式
 #define PIT_CONTROL_PORT    0x43                                //8253控制寄存器端口
 
+uint32_t ticks;
 /* 把操作的计数器counter_no、读写锁属性rwl、计数器模式counter_mode写入模式控制寄存器并赋予初始值counter_value 
     rwl: pic读写方式
 */
@@ -27,9 +31,23 @@ static void frequency_set(uint8_t counter_port, \
     outb(counter_port, (uint8_t)(counter_port >>8));
     }
 
+/* 时钟的中断处理函数 */
+static void intr_timer_handler(void){
+    struct task_struct* cur_thread = running_thread();
+    ASSERT(cur_thread->stack_magic == 0x20020905);
+    ticks++;
+    if (cur_thread->ticks == 0 )
+    {
+        schedule();
+    }else{
+        cur_thread->ticks--;
+    }
+}
+
 void timer_init(){
     put_str("timer_init start\n");
     /* 设置8253的定时周期,也就是发中断的周期 */
     frequency_set(CONTRER0_PORT, COUNTER0_NO, READ_WRITE_LATCH, COUNTER_MODE, COUNTER0_VALUE);
+    register_handler(0x20, intr_timer_handler);
     put_str("timer_init done\n");
 }
