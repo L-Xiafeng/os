1cda87373c05d12880855e036632479c13907100
L-Xiafeng <q1612188282@gmail.com>
2022-03-04 13:43:37 +0800

    feat(syscall): add malloc and free for userprocess

--- lib/user/syscall.h
+++ lib/user/syscall.h
@@ -4,8 +4,11 @@
 enum SYSCALL_NR {
    SYS_GETPID,
    SYS_WRITE,
-   SYS_PUT_CHAR
+   SYS_MALLOC,
+   SYS_FREE   
 };
 uint32_t getpid(void);
 uint32_t write(char* str);
+void* malloc(uint32_t size);
+void free(void* ptr);
 #endif
